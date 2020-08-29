2839b5e79aed342242bfa353a86c9370fcddd1d4
CC <50982416+cyndereN@users.noreply.github.com>
2020-08-29 01:28:04 +0100

    Update htmls

--- Memo1/To_do/views.py
+++ Memo1/To_do/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 tasks = ["111", "222", "333"]
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html", {
+    return render(request, "To_do/index.html", {
         "tasks": tasks,
     })
 
 def add(request):
-    return render(request, "task/add.html", )
+    return render(request, "To_do/add.html", )
