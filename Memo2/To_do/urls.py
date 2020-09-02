35546d815b0241223a4699dd50c581cbcc3c7db7
CC <50982416+cyndereN@users.noreply.github.com>
2020-09-02 16:24:03 +0100

    Update Memo2

--- Memo2/To_do/urls.py
+++ Memo2/To_do/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "To_do"
+
 urlpatterns = [
-    
+    path("", views.index, name="index"),
+    path("<int:topic_id>", views.topic, name = "topic"),
 ]
--- Memo2/To_do/views.py
+++ Memo2/To_do/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
+def index(request):
+    topics = Topic.objects.order_by('priority')
+    return render(request, 'To_do/index.html', {
+        'topics': topics,
+    })
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    return render(request, 'To_do/topic.html', {
+        'topic': topic, 
+        'entries': entries
+    })
+
